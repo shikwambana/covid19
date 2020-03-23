1d37d7a122a1932cd433933b526751a48796d264
Alphie Shikwambana <alphie.shikwambana@neutrinos.co>
2020-03-23 14:45:08 +0200

    added graph, info component and article component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ArticleComponent } from './article/article.component';
+import { GraphsComponent } from './graphs/graphs.component';
+import { InfoComponent } from './info/info.component';
 
 
 const routes: Routes = [
   { path: '', component : HomeComponent},
-  { path: 'article/:articleID/:articleTitle', component: ArticleComponent}
+  { path: 'article/:articleID/:articleTitle', component: ArticleComponent},
+  { path: 'thenumbers', component: GraphsComponent},
+  { path: 'information', component: InfoComponent, children : [
+    { path: ':section', component: InfoComponent}
+  ]}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'; 
-import { MatCardModule, MatAutocompleteModule, MatIconModule } from '@angular/material';
+import { MatCardModule, MatAutocompleteModule, MatMenuModule, MatIconModule, MatTabsModule, MatExpansionModule, MatChipsModule } from '@angular/material';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ArticlesComponent } from './articles/articles.component';
 import { ArticleComponent } from './article/article.component';
+import { GraphsComponent } from './graphs/graphs.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ArticlesComponent,
-    ArticleComponent
+    ArticleComponent,
+    GraphsComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
@@ -25,6 +29,10 @@ import { ArticleComponent } from './article/article.component';
     MatCardModule,
     MatAutocompleteModule,
     MatIconModule,
+    MatTabsModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatMenuModule,
     FormsModule, 
     ReactiveFormsModule
   ],
